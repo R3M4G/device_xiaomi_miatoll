09ef5a0479a463cfafd7df32a2cf96bfa9abf9ed
itsxrp <itsxrproms@gmail.com>
2021-06-21 05:43:58 +0000

    miatoll: Bringup RR
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- rr_miatoll.mk
+++ rr_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C)
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,20 +7,17 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common RR stuff
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := rr_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
